073b75cbef177d8eefdc814db74c999efb842a2d
Alexander Liu <alexander_l_22@hotmail.com>
2024-11-15 09:21:40 -0500

    Add argument parsing

---
+++ fft.py
@@ -0,0 +1,18 @@
+import argparse
+import utils
+
+def parse_input():
+    parser = argparse.ArgumentParser(description='FFT Application')
+    parser.add_argument('-m', type=utils.validate_mode)
+    parser.add_argument('-i', type=utils.validate_image_filename)
+    return parser.parse_args()
+
+def main():
+    # Parse user inputs
+    args = parse_input()
+    # Obtain mode and image
+    mode = args.m
+    image = args.i
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+def validate_mode(value):
+    try:
+        mode = int(value)
+
+        if mode not in [1, 2, 3, 4]:
+            print("ERROR\tInvalid mode, select from [1, 2, 3, 4]")
+            sys.exit(1)
+
+        return mode
+
+    except ValueError:
+        print("ERROR\tMode must be a valid integer")
+        sys.exit(1)
+
+def validate_image_filename(value):
+    if not _is_valid_file(value):
+        print("ERROR\tFile name not found in directory")
+
+    if not _is_valid_image_file(value):
+        print("ERROR\tInvalid file extension detected (must be .png)")
+
+    return value
+
+# Private helper function to validate if provided file name exists in cwd
+def _is_valid_file(filename):
+    return os.path.isfile(os.path.join(os.getcwd(), filename))
+
+# Private helper function to validate if file extension is a valid image extension
+def _is_valid_image_file(filename):
+    # Can add more valid extensions in the future
+    valid_extensions = ['.png']
+    _, ext = os.path.splitext(filename.lower())
+    return ext in valid_extensions
